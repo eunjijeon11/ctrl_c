5e859d3616fcb69c5c83c153b9e3e8771c9ff37b
전또맹 <eunjiamy@naver.com>
2020-09-12 03:12:33 +0900

    add activity&fragment

---
+++ app/src/main/java/com/example/ctrl_c/subject_setting.java
@@ -0,0 +1,14 @@
+package com.example.ctrl_c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class subject_setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_setting);
+    }
+}
---
+++ app/src/main/java/com/example/ctrl_c/timetable_setting.java
@@ -0,0 +1,14 @@
+package com.example.ctrl_c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class timetable_setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timetable_setting);
+    }
+}
