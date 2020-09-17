a6cbcc2f206f2c01ccfb1db16a9eba0dfd3f649e
전또맹 <eunjiamy@naver.com>
2020-09-17 13:19:15 +0900

    메인화면 프래그맨트 연결 각자 화면 볼 수 있음

--- app/src/main/java/com/example/ctrl_c/MainActivity.java
+++ app/src/main/java/com/example/ctrl_c/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.ctrl_c;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    frag_alarm frag_alarm;
+    frag_timetable frag_timetable;
+    frag_setting frag_setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.alarm:
+                        setFrag(0);
+                        break;
+                    case R.id.timetable:
+                        setFrag(1);
+                        break;
+                    case R.id.setting:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+        frag_alarm = new frag_alarm();
+        frag_timetable = new frag_timetable();
+        frag_setting = new frag_setting();
+        setFrag(0);
+
+    }
+
+    private void setFrag(int n) {
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch (n) {
+            case 0:
+                fragmentTransaction.replace(R.id.frame_main, frag_alarm);
+                fragmentTransaction.commit();
+                break;
+            case 1:
+                fragmentTransaction.replace(R.id.frame_main, frag_timetable);
+                fragmentTransaction.commit();
+                break;
+            case 2:
+                fragmentTransaction.replace(R.id.frame_main, frag_setting);
+                fragmentTransaction.commit();
+                break;
+        }
     }
 }
