53aac9f32958fbf6d52a755fe78752ccce112cb3
전또맹 <eunjiamy@naver.com>
2021-03-01 00:47:26 +0900

    Revert "Revert "Merge pull request #30 from eunjijeon11/Jihyeon_#11""
    This reverts commit 3fc8bd09d9a2242bf9fcdbc3617d03959c55dc88.

--- app/src/main/java/com/example/ctrl_c/frag_alarm.java
+++ app/src/main/java/com/example/ctrl_c/frag_alarm.java
@@ -1,10 +1,12 @@
 package com.example.ctrl_c;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
@@ -13,12 +15,16 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class frag_alarm extends Fragment {
 
     View view;
     RecyclerView alarmRecyclerView;
     alarm_RecyclerViewAdapter a_Rvadapter;
     Button btn_add;
+    DBOpenHelper dbOpenHelper;
+    ArrayList<Long> arrayIndex = new ArrayList<>();
 
     @Nullable
     @Override
@@ -36,7 +42,7 @@ public class frag_alarm extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(),alarm_setting.class);
-                intent.putExtra("Change type", "Add");
+                intent.putExtra("Change type", "add");
                 startActivityForResult(intent,1000);
             }
         });
@@ -46,8 +52,11 @@ public class frag_alarm extends Fragment {
             @Override
             public void onChangeClick(int position) {
                 Intent intent = new Intent(getActivity(),alarm_setting.class);
-                intent.putExtra("Change type", "Change");
-                intent.putExtra("change Position",position);
+                intent.putExtra("Change type", "change");
+                intent.putExtra("alarm id", arrayIndex.get(position));
+                intent.putExtra("alarm Name", a_Rvadapter.items.get(position).getAlarmName());
+                intent.putExtra("alarm Hour", a_Rvadapter.items.get(position).getHour());
+                intent.putExtra("alarm Min", a_Rvadapter.items.get(position).getMin());
                 startActivityForResult(intent,1000);
             }
 
@@ -58,13 +67,31 @@ public class frag_alarm extends Fragment {
             }
         });
 
-        AlarmData alarmData = new AlarmData();
-        alarmData.setAlarmName("학교 갈 시간");
-        alarmData.setHour(6);
-        alarmData.setMin(30);
-        alarmData.setOnOff(true);
-        a_Rvadapter.addItem(alarmData);
-        a_Rvadapter.notifyDataSetChanged();
+
+        dbOpenHelper = new DBOpenHelper(getActivity());
+        dbOpenHelper.open("alarm");
+
+        Cursor cursor = dbOpenHelper.selectColumns("alarm");
+        while (cursor.moveToNext()) {
+            long tempIndex = cursor.getLong(cursor.getColumnIndex("_id"));
+            String tempAlarmName = cursor.getString(cursor.getColumnIndex("alarm name"));
+            int tempHour = cursor.getInt(cursor.getColumnIndex("alarm hour"));
+            int tempMin = cursor.getInt(cursor.getColumnIndex("alarm min"));
+            boolean tempOnOff = (cursor.getInt(cursor.getColumnIndex("onOff")) == 1); //숫자인 onOff를 boolean으로 만들기
+
+            //...가져온 데이터를 이용하기(리사이클러뷰에 추가하기 등등)...
+            AlarmData tempData = new AlarmData();
+            tempData.setAlarmName(tempAlarmName);
+            tempData.setHour(tempHour);
+            tempData.setMin(tempMin);
+            tempData.setOnOff(tempOnOff);
+            a_Rvadapter.addItem(tempData);
+
+            //id 저장하기
+            arrayIndex.add(tempIndex);
+        }
+        cursor.close();
+
 
         return view;
 
@@ -88,10 +115,12 @@ public class frag_alarm extends Fragment {
                     tempData.setHour(data.getIntExtra("alarm hour", 0));
                     tempData.setMin(data.getIntExtra("alarm min", 0));
                     tempData.setOnOff(true);
-                    a_Rvadapter.items.add(data.getIntExtra("alarm position", 0), tempData);
+                    a_Rvadapter.items.add(arrayIndex.indexOf(data.getIntExtra("alarm position", 0)), tempData);
                     a_Rvadapter.notifyDataSetChanged();
                 }
             }
         }
     }
+
+
 }
